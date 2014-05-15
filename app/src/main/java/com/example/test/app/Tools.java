676c803519a317d22011abfee2d559eee7708a86
xiaohu409 <285279904@qq.com>
2014-05-15 20:26:57 +0800

    修改了字符串的逻辑处理算法
    +         int i = strBuf.length();
    +         if (i % 2 == 0) {
    +             i = (i - 1) / 2;
    +         }
    +         else {
    +             i = i / 2;
    +         }

--- app/src/main/java/com/example/test/app/Tools.java
+++ app/src/main/java/com/example/test/app/Tools.java
@@ -1,19 +1,25 @@
 package com.example.test.app;
 
+
 /**
  * Created by hutao on 2014-5-14.
  */
 public class Tools {
 
     public static String operateString(String valStr, char c) {
-        StringBuilder strBuf = new StringBuilder(valStr);
-        int i = strBuf.length() / 2;
         int index;
+        StringBuilder strBuf = new StringBuilder(valStr);
+        int i = strBuf.length();
+        if (i % 2 == 0) {
+            i = (i - 1) / 2;
+        }
+        else {
+            i = i / 2;
+        }
         for (int j = 1; j <= i; j++) {
             index = 3 * j - 1;
             strBuf.insert(index, c);
         }
         return strBuf.toString();
     }
-
 }
